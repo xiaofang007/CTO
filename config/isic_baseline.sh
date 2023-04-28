084b4e865cd52dd7949f660c43789b487d6cff2f
fangxiao <xfangal@connect.ust.hk>
2023-04-28 15:19:31 +0800

    baseline config for isic train and test

--- config/isic_baseline.sh
+++ config/isic_baseline.sh
@@ -1,9 +1,8 @@
-GPU=1 6
 MAX_FOLD=4
 cd ../CTOTrainer
 
 # FOLD 0-4
 for fold_id in $(seq 0 1 $MAX_FOLD)
 do  
-python train.py --task baseline --fold $fold_id --train-gpus $GPU --train-lr 0.0001 --train-train-epochs 100
+python train.py --task baseline --fold $fold_id --train-train-epochs 90 --train-gpus 1 5   --train-lr 0.0001 --train-workers=4
 done
---
+++ config/isic_test_baseline.sh
@@ -0,0 +1,8 @@
+MAX_FOLD=4
+cd ../CTOTrainer
+
+# FOLD 0-4
+for fold_id in $(seq 0 1 $MAX_FOLD)
+do  
+python test.py --task baseline --fold $fold_id  --test-gpus 1
+done
